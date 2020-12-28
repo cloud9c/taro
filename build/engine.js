45b9ba6e7d2cc72975be172c2b2484f432aee6da
Cloud9c <kire5030@gmail.com>
2020-12-27 20:23:54 -0500

    Create engine.js

---
+++ build/engine.js
@@ -0,0 +1,10 @@
+export * from "./lib/three.js";
+export { Entity } from "./core/Entity.js";
+export { Scene } from "./core/Scene.js";
+export { Application } from "./core/Application.js";
+export { _components, createComponent } from "./core/Component.js";
+
+export { PhysicMaterial } from "./physics/PhysicMaterial.js";
+export { SpringDamper } from "./physics/SpringDamper.js";
+export { AngularLimit } from "./physics/AngularLimit.js";
+export { LinearLimit } from "./physics/LinearLimit.js";
