93c8687d896a0cdbc695d689eeeeb477959a8d13
Cloud9c <kire5030@gmail.com>
2021-03-09 00:02:17 -0500

    add taro.min.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import compiler from '@ampproject/rollup-plugin-closure-compiler';
+
 export default [
 	{
 		input: './src/Taro.js',
@@ -7,6 +9,16 @@ export default [
 			name: 'TARO',
 			indent: '\t'
 		}
+	},
+	{
+		input: './src/Taro.js',
+		output: {
+			file: './build/taro.min.js',
+			format: 'umd',
+			name: 'TARO',
+			indent: '\t'
+		},
+		plugins: [ compiler() ]
 	},
 	{
 		input: './src/Taro.js',
