a7e2e902bd14cea653ec8ec964b6be9227653542
Cloud9c <kire5030@gmail.com>
2021-03-10 12:45:09 -0500

    build

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,40 @@
 import compiler from '@ampproject/rollup-plugin-closure-compiler';
+import { babel } from '@rollup/plugin-babel';
+
+const babelrc = {
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				modules: false,
+				targets: '>1%',
+				loose: true,
+				bugfixes: true,
+			}
+		]
+	]
+};
+
+function babelCleanup() {
+
+	const doubleSpaces = / {2}/g;
+
+	return {
+
+		transform( code ) {
+
+			code = code.replace( doubleSpaces, '\t' );
+
+			return {
+				code: code,
+				map: null
+			};
+
+		}
+
+	};
+
+}
 
 export default [
 	{
@@ -8,7 +44,16 @@ export default [
 			format: 'umd',
 			name: 'TARO',
 			indent: '\t'
-		}
+		},
+		plugins: [
+			babel( {
+				babelHelpers: 'bundled',
+				compact: false,
+				babelrc: false,
+				...babelrc
+			} ),
+			babelCleanup()
+		]
 	},
 	{
 		input: './src/Taro.js',
@@ -18,7 +63,16 @@ export default [
 			name: 'TARO',
 			indent: '\t'
 		},
-		plugins: [ compiler() ]
+		plugins: [
+			babel( {
+				babelHelpers: 'bundled',
+				compact: false,
+				babelrc: false,
+				...babelrc
+			} ),
+			babelCleanup(),
+			compiler()
+		]
 	},
 	{
 		input: './src/Taro.js',
